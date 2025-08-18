65654dead00a3819af6475b04fac075991249981
thiagopls1 <thiagopls1@hotmail.com>
2025-08-17 23:55:51 -0300

    Add sum to custom types

--- src/custom_types.nim
+++ src/custom_types.nim
@@ -12,13 +12,21 @@ when isMainModule:
   converter toJarda(c: Centimetro): Jarda = Jarda(float(c) / 91.44)
   converter toJarda(p: Polegada): Jarda = Jarda(float(p) / 36)
 
-  # Declaração de operadores (Nesse caso, formatação de string)
+  # Declaração de operadores
+  # Nas somas, definimos como padrão de retorno o tipo com menor valor
+  proc `+`(a: Polegada, b: Centimetro): Centimetro = Centimetro(float(toCm(a)) + float(b))
+  proc `+`(a: Jarda, b: Centimetro): Centimetro = Centimetro(float(toCm(a)) + float(b))
+  proc `+`(a: Jarda, b: Polegada): Polegada = Jarda(float(toPol(a)) + float(b))
   proc `$`(c: Centimetro): string = $float(c) & " cm"
   proc `$`(p: Polegada): string = $float(p) & " pol"
   proc `$`(j: Jarda): string = $float(j) & " jd"
 
   let centimetro = Centimetro(196.30)
+  let polegada = Polegada(28)
+  let jarda = Jarda(28)
 
-  echo(centimetro)
+  echo("centimetro + polegada: ", centimetro + polegada)
+  echo("jarda + polegada: ", jarda + polegada)
+  echo("jarda + centimetro: ", jarda + centimetro)
   echo(toPol(centimetro))
   echo(toJarda(centimetro))
