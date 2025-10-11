70bb6a8e707f6f494fe12fbf3558f3e6e853a8fa
viniciusegydio <egydiovinicius022@gmail.com>
2025-10-11 16:28:49 -0300

    adds isMainModule

--- src/trabalho_2/polimorfism_and_high_order.nim
+++ src/trabalho_2/polimorfism_and_high_order.nim
@@ -1,8 +1,9 @@
 proc aplicarDuasVezes[T](x: T, f: proc(p: T): T): T = #T se refere a um tipo genérico, que será recebido e retornado
  return f(f(x)) 
 
-proc double(x: int): int = x * 2
-echo aplicarDuasVezes(3, double) #Deve retornar 12
-
-proc exclamacao(s: string): string = s & "!" #& é o operador de concatenação do Nim
-echo aplicarDuasVezes("Oi", exclamacao) #Deve retornar um "Oi!!"
+when isMainModule:
+ proc double(x: int): int = x * 2
+ echo aplicarDuasVezes(3, double) #Deve retornar 12
+ 
+ proc exclamacao(s: string): string = s & "!" #& é o operador de concatenação do Nim
+ echo aplicarDuasVezes("Oi", exclamacao) #Deve retornar um "Oi!!"
