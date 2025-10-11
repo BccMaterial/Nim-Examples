30b2f48e8effdfb569b6719585f75dac521503be
thiagopls1 <thiagopls1@hotmail.com>
2025-10-11 15:27:58 -0300

    refactor: declare functions for better reading

--- src/trabalho_2/function_as_argument.nim
+++ src/trabalho_2/function_as_argument.nim
@@ -1,15 +1,12 @@
 when isMainModule:
-  # Define duas funções simples
   proc soma(x, y: int): int = x + y
   proc multiplica(x, y: int): int = x * y
   
-  # Define uma função que recebe outra função como parâmetro
   proc calculadora(
     a, b: int, 
     operacao: proc(x, y: int): int
   ): int =
     return operacao(a, b)
   
-  # Usa a função 'calculadora' com diferentes operações
   echo("5 + 3 = ", calculadora(5, 3, soma))
   echo("5 * 3 = ", calculadora(5, 3, multiplica))
--- src/trabalho_2/high_order.nim
+++ src/trabalho_2/high_order.nim
@@ -4,17 +4,27 @@ import std/sequtils
 # filter -> filterIt
 # reduce -> foldl
 
+func soma(lista: seq[int]): int =
+  return foldl(lista, a + b)
+
+func dobrar_itens(lista: seq[int]): seq[int] =
+  return lista.mapIt(it * 2)
+
+func filtrar_pares(lista: seq[int]): seq[int] =
+  return lista.filterIt(it mod 2 == 0)
+
 when isMainModule:
   let numeros = @[1, 2, 3, 4, 5, 7, 8, 9, 10]
+
   # Calcula o dobro
-  let dobro = numeros.mapIt(it * 2)
+  let dobro = dobrar_itens(numeros)
   echo("Lista * 2: ", dobro)
 
   # Filtra os pares
-  let pares = numeros.filterIt(it mod 2 == 0)
+  let pares = filtrar_pares(numeros)
   echo("Lista pares: ", pares)
 
   # Soma os itens
-  let soma = foldl(numeros, a + b)
-  echo("Soma numeros: ", soma)
+  let total = soma(numeros)
+  echo("Soma numeros: ", total)
 
