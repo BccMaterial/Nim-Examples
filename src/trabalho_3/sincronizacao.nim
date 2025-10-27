db8778b3d5d2717ad9d10a5c0dd94129c4800668
thiagopls1 <thiagopls1@hotmail.com>
2025-10-26 21:25:09 -0300

    fix `mutex` at `sincronizacao`

--- src/trabalho_3/sincronizacao.nim
+++ src/trabalho_3/sincronizacao.nim
@@ -1,13 +1,14 @@
 import std/locks, std/threadpool
 
-#Sincronização por Locks
-
-var cont = 0 #Compartilhado entre threads
-var mutex = Lock #Protege o acesso ao contador
-initLock(mutex) #inicialização do mutex
+# Sincronização por Locks
+var cont = 0 # Compartilhado entre threads
+var mutex: Lock # Protege o acesso ao contador
+initLock(mutex) # Inicialização do mutex
 
 proc task(n:int) {.thread.} =
   for i in 1..1000:
-    acquire(mutex) #Bloqueia o mutex, gerando acesso exclusivo a thread atual para a seção crítica
+    # Bloqueia o mutex, gerando acesso exclusivo a thread atual para a seção crítica
+    acquire(mutex)
     cont += 1
-    release(mutex) #Faz a liberação da mutex, permitindo que outra thr
+    # Faz a liberação da mutex, permitindo que outra thread seja executada
+    release(mutex)
